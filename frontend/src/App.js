16281d209265da70ea7a29b2cf47a89d2f01b179
Matthew Charters <matthew@charitycan.ca>
2023-04-27 11:30:54 -0400

    Send something to the new API endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function App() {
-  const [getMessage, setGetMessage] = useState({});
   const [theme, setTheme] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api")
-      .then((response) => {
-        console.log("SUCCESS", response);
-        setGetMessage(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch(`http://localhost:5000/api/theme/${theme}`)
+    fetch(`http://localhost:5000/api/${theme}`)
       .then((response) => {
         console.log("SUCCESS", response);
-        setGetMessage(response);
+        setTheme(response);
       })
       .catch((error) => {
         console.log(error);
@@ -46,11 +34,6 @@ function App() {
             />
             <button type="submit">Fetch Theme</button>
           </form>
-          <div>{getMessage.status === 200 ? 
-            <h3>{getMessage.status}</h3>
-            :
-            <h3>LOADING</h3>}
-          </div>
         </Col>
       </Row>
     </Container>
