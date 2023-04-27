fd4cf13d788686ebcd64b5d6b6a8420e4dafc9ba
A-Banjo <grace@charitycan.com>
2023-04-27 11:22:26 -0400

    input field that fetches endpoint w.theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState } from 'react';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [getMessage, setGetMessage] = useState({})
+  const [getMessage, setGetMessage] = useState({});
+  const [theme, setTheme] = useState("");
 
-  useEffect(()=>{
-    fetch('http://localhost:5000/api').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then((response) => {
+        console.log("SUCCESS", response);
+        setGetMessage(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:5000/api/theme/${theme}`)
+      .then((response) => {
+        console.log("SUCCESS", response);
+        setGetMessage(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ? 
-          <h3>{getMessage.status}</h3>
-          :
-          <h3>LOADING</h3>}</div>
+        <h1>GPT Crossword</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter theme"
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+          />
+          <button type="submit">Fetch Theme</button>
+        </form>
+        <div>
+          {getMessage.status === 200 ? (
+            <h3>{getMessage.status}</h3>
+          ) : (
+            <h3>LOADING</h3>
+          )}
+        </div>
       </header>
     </div>
   );
