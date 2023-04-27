c318ae383debd5dad08147337293143d3b8fbb94
Matthew Charters <matthew@charitycan.ca>
2023-04-27 10:53:46 -0400

    Contact the API for a response

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/api').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.status}</h3>
+          :
+          <h3>LOADING</h3>}</div>
       </header>
     </div>
   );
