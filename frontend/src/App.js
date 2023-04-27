5904abb91f691bdd1925d9568ec405a32c08e526
Matthew Charters <matthew@charitycan.ca>
2023-04-27 11:22:18 -0400

    Add bootstrap for stylez

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   const [getMessage, setGetMessage] = useState({})
@@ -15,16 +16,18 @@ function App() {
 
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ? 
-          <h3>{getMessage.status}</h3>
-          :
-          <h3>LOADING</h3>}</div>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>React + Flask Tutorial</h1>
+          <div>{getMessage.status === 200 ? 
+            <h3>{getMessage.status}</h3>
+            :
+            <h3>LOADING</h3>}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
